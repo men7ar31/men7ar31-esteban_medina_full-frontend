e1316ed7a5a57acf49bd1161376f27e980de39af
Nico <medinanico93@gmail.com>
2025-03-20 13:50:48 -0300

    ci: configuracion para deploy en vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',  // Asegura que los assets se carguen correctamente en Vercel
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true, // Abre automáticamente el navegador en desarrollo
+  },
+  preview: {
+    port: 4173, // Asegura que el preview de Vite use un puerto distinto
+  }
 })
